b3007b989538bf18cfd2f525eff9132935ea3ddf
Mason Lin (賣森林) <104554013@cc.ncu.edu.tw>
2015-09-08 14:05:56 +0800

    Update node_express_test.js

--- node_express_test.js
+++ node_express_test.js
@@ -5,9 +5,15 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+/*
 var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+*/
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', server.address().port);
 });
